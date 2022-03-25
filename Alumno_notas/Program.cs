dfe8daf2eab1ac2aa4fb3f9765ce4f1b899552df
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:27:40 -0600

    Agregar archivos de proyecto.

---
+++ Alumno_notas/Program.cs
@@ -0,0 +1,25 @@
+﻿int can = 0;
+int var = 0;
+int suma = 0;
+string nombre;
+Console.WriteLine("Introduce el nombre del alumno");
+nombre = Console.ReadLine();
+Console.WriteLine("Cuantas notas: ");
+can = int.Parse(Console.ReadLine());
+int[] vec = new int[can + 1];
+
+int k = 0;
+while (k < can)
+{
+    do
+    {
+        Console.WriteLine("Introduce una nota: ");
+        var = int.Parse(Console.ReadLine());
+
+    } while (var < 1 || var > 100);
+    vec[k] = var;
+    suma += vec[k];
+    k++;
+}
+Console.WriteLine("Fin del programa");
+Console.ReadKey(true);
